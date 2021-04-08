6fbdfaf9dfc5e1e05038c13d25d4e1a38c7bdebc
Imam Robani <imamrob@gmail.com>
2021-04-08 09:18:07 +0700

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/theme.dart';
+import 'package:flutter_basic/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Basic'),
-        ),
-        body: Image.asset(
-          'assets/images/primary.png',
-        ),
+      home: HomePage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
     );
   }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Profile Picture',
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.asset(
+                'assets/images/primary.png',
+                width: 140,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Anne Margaritha',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                'UX Desiginer',
+                style: TextStyle(
+                  color: greyColor,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Wrap(
+                spacing: 38,
+                runSpacing: 40,
+                children: [
+                  Image.asset(
+                    'assets/images/item1.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/item2.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/item3.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/item4.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/item5.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/item6.png',
+                    width: 80,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
